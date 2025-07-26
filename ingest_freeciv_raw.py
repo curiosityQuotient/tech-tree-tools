6cf6ad6f875a67e81f7c8fafae1d5f1de77a5826
CuriosityQuotient <CuriosityQuotient>
2025-07-26 22:29:01 +0100

    feat: inital data and code

---
+++ ingest_freeciv_raw.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+fp = "freeciv_raw.csv"
+
+tech_df = pd.read_csv(fp)
+tech_df = tech_df.fillna("None")
+
+tech_df["Type"] = "requirement"
+
+tech_filter = tech_df["Description"].str.contains("Total tech")
+tech_df.loc[tech_filter, "Type"] = "dependent"
+
+df_copy = tech_df.copy().reset_index(drop=True)
+
+# Find which A each row belongs to by forward-filling A indices
+a_indices = df_copy[df_copy["Type"] == "dependent"].index
+df_copy["a_group"] = None
+
+current_a_idx = None
+for idx in df_copy.index:
+    if df_copy.loc[idx, "Type"] == "dependent":
+        current_a_idx = idx
+    df_copy.loc[idx, "a_group"] = current_a_idx
+
+# Group B strings by their corresponding A
+b_strings_by_a = {}
+for idx, row in df_copy.iterrows():
+    if row["Type"] == "requirement" and row["a_group"] is not None:
+        a_idx = row["a_group"]
+        if a_idx not in b_strings_by_a:
+            b_strings_by_a[a_idx] = []
+        b_strings_by_a[a_idx].append(row["Tech requirements"])
+
+# Create result with only A rows
+result = df_copy[df_copy["Type"] == "dependent"].copy()
+
+# Add B strings as lists (or join them as single strings)
+result["b_strings_list"] = result.index.map(lambda x: b_strings_by_a.get(x, []))
+
+# Also create a joined version
+result["b_strings_joined"] = result["b_strings_list"].apply(
+    lambda x: "".join(x) if x else ""
+)
+
+result["b_strings_joined"] = result["b_strings_joined"].str.replace(",", ";")
+
+output_df = result.loc[:, ["Tech requirements", "b_strings_joined"]]
+output_df = output_df.rename(
+    columns={"Tech requirements": "Name", "b_strings_joined": "Predecessors"}
+)
+output_df["Successors"] = ""
+
+output_df.to_csv("freeciv_tech_tree.csv", index=False)
+
+print("x")
---
+++ tech_tree.py
@@ -0,0 +1,78 @@
+# Test of a tech tree visualiser tool
+import csv
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+class TechTree:
+    """Class for technology tree."""
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def from_csv(self, csv_path: str):
+        """Populates a technology tree object from a csv."""
+        # Import data from csv
+        with open(raw_csv_path) as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=",", quotechar='"')
+            headers = next(spamreader)
+            raw_csv_data = []
+            for row in spamreader:
+                node = {
+                    headers[0]: row[0],
+                    headers[1]: row[1].split(";"),
+                    headers[2]: row[2].split(";"),
+                }
+                raw_csv_data.append(node)
+
+        # create nodes
+        tech_tree = nx.DiGraph()
+        tech_list = []
+        for tech in raw_csv_data:
+            tech_list.append(
+                (
+                    tech[headers[0]],
+                    {headers[1]: tech[headers[1]], headers[2]: tech[headers[2]]},
+                )
+            )
+            tech_tree.add_node(
+                tech[headers[0]],
+                **{headers[1]: tech[headers[1]], headers[2]: tech[headers[2]]},
+            )
+        # add edges
+        for tech in raw_csv_data:
+            current_tech = tech[headers[0]]
+            prereqs = tech[headers[1]]
+            for prereq in prereqs:
+                if prereq:  # skip empty strings
+                    tech_tree.add_edge(prereq, current_tech)
+
+        self.tech_graph = tech_tree
+
+    def draw_graph(self):
+        """Draws the graph that has been created."""
+        # generation calcuations
+        gen_info = [gen for gen in nx.topological_generations(self.tech_graph)]
+        width = len(gen_info)
+        height = max([len(gen) for gen in gen_info])
+
+        print(f"Width:{width}, Height:{height}")
+
+        offset = height - 1
+        posns = {}
+        for ii, techs in enumerate(gen_info):
+            # ii is width
+            horz = ii
+            for jj, tech in enumerate(techs):
+                vert = jj + offset
+                posns[tech] = (ii, jj)
+
+        nx.draw(self.tech_graph, posns, with_labels=True)
+        plt.show()
+
+
+if __name__ == "__main__":
+    tech_tree = TechTree("Freeciv technology tree")
+    raw_csv_path = "freeciv_tech_tree.csv"  # "tech_tree_example.csv"
+    tech_tree.from_csv(raw_csv_path)
+    tech_tree.draw_graph()
