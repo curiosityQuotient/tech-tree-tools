dfe540e76073563119e758c150c65e8917f7f464
CuriosityQuotient <33955013+curiosityQuotient@users.noreply.github.com>
2025-09-07 08:20:31 +0100

    test: added tests for 3 features

--- tests/test_tech_tree.py
+++ tests/test_tech_tree.py
@@ -13,3 +13,28 @@ def test_from_csv():
     assert "A" in tech_tree.tech_graph.nodes
     assert "B" in tech_tree.tech_graph.nodes
     assert "C" in tech_tree.tech_graph.nodes
+
+def test_list_predecessors():
+    """Test list predecessors functionality."""
+    tech_tree = TechTree("Minimal Tech Tree")
+    tech_tree.from_csv("tests/fixtures/minimal.csv")
+    predecessor_list = tech_tree.list_predecessors("C")
+    assert predecessor_list == ["A", "B"]
+
+def test_set_progress():
+    """Test that progress is set correctly"""
+    tech_tree = TechTree("Minimal Tech Tree")
+    tech_tree.from_csv("tests/fixtures/minimal.csv")
+    tech_tree.set_progress(["B"])
+    assert tech_tree.tech_graph.nodes["B"]["completed"]
+    assert tech_tree.tech_graph.nodes["A"]["completed"]
+    assert ~tech_tree.tech_graph.nodes["C"]["completed"]
+
+def test_path_to_target():
+    """Test path to target calculation."""
+    tech_tree = TechTree("Minimal Tech Tree")
+    tech_tree.from_csv("tests/fixtures/minimal.csv")
+    path_tree = tech_tree.path_to_target("B")
+    assert "A" in path_tree.tech_graph.nodes
+    assert "B" in path_tree.tech_graph.nodes
+    assert "C" not in path_tree.tech_graph.nodes
